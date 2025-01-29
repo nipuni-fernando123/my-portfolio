1bd72a4eb43e2112bae19f8c9df0a4e4345471c8
Sachintha Chathuranga <smschathu1234@gmail.com>
2025-01-29 14:50:44 +0530

    Update route path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-       <div className=" flex justify-center  lg:justify-end mt-8 mb-4 lg:mr-8">
-    
-      <ul className="flex text-white font-semibold  text-sm lg:text-2xl ">
-        <li><a href="/" className="px-6 no-underline hover:text-yellow-500 mb-2">Home</a></li>
-        <li><a href="/about" className="px-6 no-underline hover:text-yellow-500  ">About</a></li>
-        <li><a href="/projects" className="px-6 no-underline hover:text-yellow-500 ">Projects</a></li>
-        <li><a href="/contact" className="px-6 no-underline hover:text-yellow-500 ">Contact</a></li>
+    <div className="flex justify-center lg:justify-end mt-8 mb-4 lg:mr-8">
+      <ul className="flex text-white font-semibold text-sm lg:text-2xl">
+        <li><Link to="/my-portfolio/" className="px-6 no-underline hover:text-yellow-500 mb-2">Home</Link></li>
+        <li><Link to="/my-portfolio/about" className="px-6 no-underline hover:text-yellow-500">About</Link></li>
+        <li><Link to="/my-portfolio/projects" className="px-6 no-underline hover:text-yellow-500">Projects</Link></li>
+        <li><Link to="/my-portfolio/contact" className="px-6 no-underline hover:text-yellow-500">Contact</Link></li>
       </ul>
-    </div>
     </div>
   );
 };
