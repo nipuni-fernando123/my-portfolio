7e3df1b5cca24c19c64d7321d53ed04c94949706
nipuni-fernando123 <nipunifernando8899@gmail.com>
2025-02-02 21:46:38 +0530

    update screen size

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,30 +10,30 @@ function About() {
     <div className="bg-blue-950 overflow-hidden h-screen">
       <div className="fixed top-0 left-0 w-full  z-10">
         <Navbar />
-        <div className="text-gray-400 text-[30px] sm:text-[40px] md:text-[50px] font-bold mt-4 text-center">
+        <div className="text-gray-400 text-[25px] sm:text-[28px] md:text-[30px] lg:text-[35px] font-bold mt-4 text-center">
           About Me
         </div>
         </div>
 
       <div className=" ">
-        <div className="mt-[160px] lg:mt-[250px] h-[calc(100vh-200px)] lg:h-auto lg:flex lg:items-center lg:justify-center overflow-y-auto lg:overflow-hidden">
+        <div className="mt-[140px] lg:mt-[180px] h-[calc(100vh-200px)] lg:h-auto lg:flex lg:items-center lg:justify-center overflow-y-auto lg:overflow-hidden">
          
 
           <div className="flex justify-center items-center  ">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
-              <div className="mx-12 lg:mx-0 justify-center items-center  bg-white xl:w-[700px] lg:w-[500px]  h-max rounded-md border-8 border-yellow-500 sm:p-8 p-4">
+              <div className="mx-16 lg:mx-0 justify-center items-center  bg-white xl:w-[600px] lg:w-[500px]  h-max rounded-md border-[6px] border-yellow-500 sm:p-8 p-4">
                 <div className="lg:flex lg:justify-between">
                   <div className="flex items-center justify-center">
                     <MdOutlineDesignServices
                       className="text-yellow-500"
-                      size={100}
+                      size={80}
                     />
                   </div>
                   <div className="px-2 sm:px-4">
-                    <div className="text-yellow-500 text-xl md:text-2xl text-center lg:text-left lg:text-3xl font-semibold pb-4">
+                    <div className="text-yellow-500 text-lg md:text-xl text-center lg:text-left lg:text-2xl font-semibold pb-4">
                       UI/UX Designer
                     </div>
-                    <div className="text-black text-md md:text-lg lg:text-xl text-justify">
+                    <div className="text-black text-sm md:text-md lg:text-[16px] text-justify">
                       UI/UX Designer skilled in creating intuitive, visually
                       engaging, and user-friendly experiences through research,
                       wireframing, and prototyping, ensuring a perfect blend of
@@ -43,57 +43,57 @@ function About() {
                 </div>
               </div>
 
-              <div className="mx-12 lg:mx-0 justify-center items-center  bg-white xl:w-[700px] lg:w-[500px] h-max rounded-md border-8 border-yellow-500 sm:p-8 p-4">
+              <div className="mx-16 lg:mx-0 justify-center items-center  bg-white xl:w-[600px] lg:w-[500px] h-max rounded-md border-[6px] border-yellow-500 sm:p-8 p-4">
                 <div className="lg:flex lg:justify-between">
                   <div className="flex items-center justify-center">
                     <FaLaptopCode
                       className="text-yellow-500"
-                      size={100}
+                      size={80}
                     />
                   </div>
                   <div className="px-2 sm:px-4">
-                    <div className="text-yellow-500 text-xl md:text-2xl text-center lg:text-left lg:text-3xl font-semibold pb-4">
+                    <div className="text-yellow-500 text-lg md:text-xl text-center lg:text-left lg:text-2xl font-semibold pb-4">
                     Frontend Developer
                     </div>
-                    <div className="text-black text-md md:text-lg lg:text-xl text-justify">
+                    <div className="text-black text-sm md:text-md lg:text-[16px] text-justify">
                     Frontend Developer passionate about building responsive, user-centric web applications by transforming design concepts into functional, high-performance, and accessible interfaces.
                     </div>
                   </div>
                 </div>
               </div>
 
-              <div className="mx-12 lg:mx-0 justify-center items-center  bg-white xl:w-[700px] lg:w-[500px] h-max rounded-md border-8 border-yellow-500 sm:p-8 p-4">
+              <div className="mx-16 lg:mx-0 justify-center items-center  bg-white xl:w-[600px] lg:w-[500px] h-max rounded-md border-[6px] border-yellow-500 sm:p-8 p-4">
                 <div className="lg:flex lg:justify-between">
                   <div className="flex items-center justify-center">
                     <BsFileCode
                       className="text-yellow-500"
-                      size={100}
+                      size={80}
                     />
                   </div>
                   <div className="px-2 sm:px-4">
-                    <div className="text-yellow-500 text-xl md:text-2xl text-center lg:text-left lg:text-3xl font-semibold pb-4">
+                    <div className="text-yellow-500 text-lg md:text-xl text-center lg:text-left lg:text-2xl font-semibold pb-4">
                     Mobile App Developer
                     </div>
-                    <div className="text-black text-md md:text-lg lg:text-xl text-justify">
+                    <div className="text-black text-sm md:text-md lg:text-[16px] text-justify">
                     Mobile App Developer specializing in designing and building high-performance Android applications with a focus on functionality, responsive design, and seamless user experience.
                     </div>
                   </div>
                 </div>
               </div>
 
-              <div className="mx-12 lg:mx-0 mb-8 lg:mb-0 justify-center items-center  bg-white xl:w-[700px] lg:w-[500px] h-max rounded-md border-8 border-yellow-500 sm:p-8 p-4">
+              <div className="mx-16 lg:mx-0 mb-8 lg:mb-0 justify-center items-center  bg-white xl:w-[600px] lg:w-[500px] h-max rounded-md border-[6px] border-yellow-500 sm:p-8 p-4">
                 <div className="lg:flex lg:justify-between">
                   <div className="flex items-center justify-center">
                     <FaPaintBrush
                       className="text-yellow-500"
-                      size={100}
+                      size={60}
                     />
                   </div>
                   <div className="px-2 sm:px-4">
-                    <div className="text-yellow-500 text-xl md:text-2xl text-center lg:text-left lg:text-3xl font-semibold pb-4">
+                    <div className="text-yellow-500 text-lg md:text-xl text-center lg:text-left lg:text-2xl font-semibold pb-4">
                     Graphic Designer
                     </div>
-                    <div className="text-black text-md md:text-lg lg:text-xl text-justify">
+                    <div className="text-black text-sm md:text-md lg:text-[16px] text-justify">
                       
                   Graphic Designer with a talent for creating visually impactful designs, specializing in branding, illustration, and visual storytelling to effectively communicate ideas and engage audiences.
                     </div>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -34,24 +34,24 @@ function Contact() {
     <div className="bg-blue-950 overflow-hidden h-screen relative">
       <div className="fixed top-0 left-0 w-full z-10">
         <Navbar />
-        <div className="text-gray-400 text-[30px] sm:text-[40px] md:text-[50px] font-bold mt-4 text-center">
+        <div className="text-gray-400 text-[25px] sm:text-[28px] md:text-[30px] lg:text-[35px] font-bold mt-4 text-center">
           Contact Me
         </div>
       </div>
 
-      <div className="mt-[120px] lg:mt-[150px] h-[calc(100vh-200px)] lg:h-auto lg:flex lg:items-center lg:justify-center overflow-y-auto lg:overflow-hidden">
-        <div className='flex justify-between items-center xl:mx-48 mx-16 mt-16'>
-          <div className='w-full h-fit bg-white rounded-md p-8 pb-8 relative'>
+      <div className="mt-[120px] lg:mt-[150px] h-[calc(105vh-200px)] lg:h-auto lg:flex lg:items-center lg:justify-center overflow-y-auto lg:overflow-hidden">
+        <div className='flex justify-between items-center mx-16 mt-2'>
+          <div className='w-full h-fit bg-white rounded-md p-4 pb-4 relative'>
             {showSuccess && <Correct onClose={() => setShowSuccess(false)} />}
             <form ref={formRef} onSubmit={onSubmit}>
               <div className='xl:flex'>
-                <input type="text" name="firstname" placeholder='Enter First Name' required className='bg-gray-300 text-md md:text-lg lg:text-xl w-full xl:w-[780px] p-2 rounded-md font-semibold text-gray-800 xl:mr-4 mt-4' required />
-                <input type="text" name="lastname" placeholder='Enter Last Name' required className='bg-gray-300 text-md md:text-lg lg:text-xl w-full xl:w-[780px] p-2 rounded-md font-semibold text-gray-800 mt-4' required />
+                <input type="text" name="firstname" placeholder='Enter First Name' required className='bg-gray-300 text-sm md:text-md lg:text-lg w-full lg:w-[450px] xl:w-[580px] p-2 rounded-md font-semibold text-gray-800 lg:mr-2 xl:mr-4 mt-2' required />
+                <input type="text" name="lastname" placeholder='Enter Last Name' required className='bg-gray-300 text-sm md:text-md lg:text-lg w-full lg:w-[450px] xl:w-[580px] p-2 rounded-md font-semibold text-gray-800 mt-2' required />
               </div>
-              <input type="email" name="email" placeholder='Enter Email Address' required className='bg-gray-300 text-md md:text-lg lg:text-xl w-full  p-2 rounded-md font-semibold text-gray-800 mt-4' required />
-              <textarea name="message" placeholder='Enter Message' required className='bg-gray-300 text-md md:text-lg lg:text-xl w-full h-[300px] p-2 rounded-md font-semibold text-gray-800 mt-4' required></textarea>
-              <div className='flex justify-end mt-4'>
-                <button type="submit" className="bg-yellow-500 w-40 md:w-50 lg:w-60 text-white px-2 py-1 md:px-4 md:py-2 rounded-lg font-semibold text-lg md:text-xl lg:text-2xl hover:bg-yellow-600">
+              <input type="email" name="email" placeholder='Enter Email Address' required className='bg-gray-300 text-sm md:text-md lg:text-lg w-full  p-2 rounded-md font-semibold text-gray-800 mt-2' required />
+              <textarea name="message" placeholder='Enter Message' required className='bg-gray-300 text-sm md:text-md lg:text-lg w-full h-[250px] p-2 rounded-md font-semibold text-gray-800 mt-2' required></textarea>
+              <div className='flex justify-end mt-2 mb-2'>
+                <button type="submit" className="bg-yellow-500 w-40 md:w-50 lg:w-60 text-white px-2 py-1 md:px-4 md:py-2 rounded-lg font-semibold text-md md:text-lg lg:text-xl hover:bg-yellow-600">
                   Send Message
                 </button>
               </div>
